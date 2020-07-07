11d927ea51c44fd6c43d9fec7efc9a691f63de55
akapkotel <btcuserbtc@gmail.com>
2020-07-07 11:31:07 +0200

    [1] Added typing type-annotations to most functions and methods. [2] Changed method of FPS counting. [3] Some variables names changed for better readability.

--- geometry.py
+++ geometry.py
@@ -2,9 +2,14 @@
 """
 Bunch of methods for 2-dimensional and 3-dimensional meshes calculations,
 especially speeds, positions, paths and level-topography-handling.
+
+TODO: fix issue #1 with case when obstacles are triangles and two top-vertices
+of obstacles and origin are placed in-line horizontally - the top vertex of
+closer triangle is omitted and an obstacle is cut.
 """
 
 import math
+from typing import List, Tuple, Sequence, Dict, Optional, Iterator
 
 from main import SCREEN_H, SCREEN_W
 
@@ -13,26 +18,27 @@ UL, UR, LL, LR = "UL", "UR", "LL", "LR"
 UPPER, LOWER, LEFT, RIGHT, DIAGONAL = "UPPER", "LOWER", "LEFT", "RIGHT", "DIAG"
 VERTICALLY, HORIZONTALLY = "VERTICALLY", "HORIZONTALLY"
 degrees = math.degrees
+hypotenuse = math.hypot
 
 
-def distance(coord_a: tuple or list, coord_b: tuple or list) -> float:
+def distance(coord_a: Tuple, coord_b: Tuple) -> float:
     """
-    Calculate distance between two segment in 2D space.
+    Calculate distance between two points in 2D space.
 
-    :param coord_a: tuple -- (x, y) coords of first point
-    :param coord_b: tuple -- (x, y) coords of second p
+    :param coord_a: Tuple -- (x, y) coords of first point
+    :param coord_b: Tuple -- (x, y) coords of second p
     :return: float -- 2-dimensional distance between segment
     """
-    return math.hypot(coord_b[0] - coord_a[0], coord_b[1] - coord_a[1])
+    return hypotenuse(coord_b[0] - coord_a[0], coord_b[1] - coord_a[1])
 
 
-def calculate_vector_2d(angle: float, scalar: float) -> tuple:
+def calculate_vector_2d(angle: float, scalar: float) -> Tuple[float, float]:
     """
     Calculate x and y parts of the current vector.
 
     :param angle: float -- angle of the vector
     :param scalar: float -- scalar value of the vector (e.g. speed)
-    :return: tuple -- x and y parts of the vector in format: (float, float)
+    :return: Tuple -- x and y parts of the vector in format: (float, float)
     """
     radians = math.radians(angle)
     change_y = math.cos(radians)
@@ -40,20 +46,22 @@ def calculate_vector_2d(angle: float, scalar: float) -> tuple:
     return change_x * scalar, change_y * scalar
 
 
-def calculate_angle(start: tuple, end: tuple) -> float:
+def calculate_angle(start: Tuple, end: Tuple) -> float:
     """
     Calculate angle in direction from 'start' to the 'end' point in degrees.
 
-    :param start: tuple -- start point coordinates (x, y)
-    :param end: tuple -- end point coordinates (x, y)
+    :param start: Tuple[float, float] -- start point coordinates (x, y)
+    :param end: Tuple[float, float] -- end point coordinates (x, y)
     :return: float -- degrees in range 0-360.
     """
     radians = -math.atan2(end[0] - start[0], end[1] - start[1])
     return degrees(radians)
 
 
-def move_along_vector(start: tuple, velocity: float, target: tuple = None,
-                      angle: float = None) -> tuple:
+def move_along_vector(start: Tuple[float, float],
+                      velocity: float,
+                      target: Optional[Tuple[float, float]] = None,
+                      angle: Optional[float] = None) -> Tuple[float, float]:
     """
     Create movement vector starting at 'start' point angled in direction of
     'target' point with scalar velocity 'velocity'. Optionally, instead of
@@ -63,11 +71,11 @@ def move_along_vector(start: tuple, velocity: float, target: tuple = None,
     angle between two segment, but want quickly calculate position of the
     another point lying on the line connecting two, known segment.
 
-    :param start: tuple -- point from vector starts
-    :param target: tuple -- target that vector 'looks at'
+    :param start: Tuple[float, float] -- point from vector starts
+    :param target: Optional[Tuple[float, float] -- target the vector 'looks at'
     :param velocity: float -- scalar length of the vector
-    :param angle: float -- angle of the vector direction
-    :return: tuple -- (optional)position of the vector end
+    :param angle: Optional[float] -- angle of the vector direction
+    :return: Tuple[float, float] -- (optional)position of the vector end
     """
     p1 = (start[0], start[1])
     if target:
@@ -83,15 +91,18 @@ def move_along_vector(start: tuple, velocity: float, target: tuple = None,
     return p1[0] + v[0], p1[1] + v[1]
 
 
-def get_intersection(p1: tuple, p2: tuple, p3: tuple, p4: tuple) -> tuple:
+def get_intersection(p1: Tuple[float, float],
+                     p2: Tuple[float, float],
+                     p3: Tuple[float, float],
+                     p4: Tuple[float, float]) -> Tuple[float, float]:
     """
     Relatively cheap method of finding segments-intersections.
 
-    :param p1: tuple -- first point of first segment
-    :param p2: tuple -- second point of first segment
-    :param p3: tuple -- first point of second segment
-    :param p4: tuple -- second point of second segment
-    :return: tuple -- position of intersection
+    :param p1: Tuple[float, float] -- first point of first segment
+    :param p2: Tuple[float, float] -- second point of first segment
+    :param p3: Tuple[float, float] -- first point of second segment
+    :param p4: Tuple[float, float] -- second point of second segment
+    :return: Tuple[float, float] -- position of intersection
     """
     x_0 = (p1[1] - p3[1])
     x_1 = (p4[1] - p3[1])
@@ -104,11 +115,11 @@ def get_intersection(p1: tuple, p2: tuple, p3: tuple, p4: tuple) -> tuple:
     return p1[0] + s * x_3, p1[1] + s * x_4
 
 
-def cross_product(a, b):
+def cross_product(a: Tuple[float, float], b: Tuple[float, float]) -> float:
     return a[0] * b[0] - b[1] * a[1]
 
 
-def ccw(points_list: list or tuple) -> bool:
+def ccw(points_list: Sequence[Tuple[float]]) -> bool:
     """
     Check if sequence of points is oriented in clockwise or counterclockwise
     order.
@@ -118,12 +129,14 @@ def ccw(points_list: list or tuple) -> bool:
     return val > 0
 
 
-def are_points_in_line(a, b, c):
+def are_points_in_line(a: Tuple[float, float],
+                       b: Tuple[float, float],
+                       c: Tuple[float, float]):
     return -EPSILON < (
             distance(a, c) + distance(c, b) - distance(a, b)) < EPSILON
 
 
-def get_polygon_bounding_box(points_list):
+def get_polygon_bounding_box(points_list: List[Tuple]) -> List[Tuple]:
     """
     Helper function for obtaining a bounding box of segment. Allows fast
     checking if two polygons intersects. It is known that if bounding boxes
@@ -136,13 +149,13 @@ def get_polygon_bounding_box(points_list):
     return box
 
 
-def get_segment_bounding_box(segment: list or tuple) -> list:
+def get_segment_bounding_box(segment: Sequence[Tuple]) -> List[Tuple]:
     """
     Helper function for obtaining a bounding box of segment. Allows fast
     checking if two segments intersects.
 
-    :param segment: list
-    return: tuple of tuples
+    :param segment: List
+    return: Tuple of tuples
     """
     box = [
         (min(segment[0][0], segment[1][0]), min(segment[0][1], segment[1][1])),
@@ -151,7 +164,10 @@ def get_segment_bounding_box(segment: list or tuple) -> list:
     return box
 
 
-def do_boxes_intersect(a: tuple, b: tuple, c: tuple, d: tuple) -> bool:
+def do_boxes_intersect(a: Tuple[float, float],
+                       b: Tuple[float, float],
+                       c: Tuple[float, float],
+                       d: Tuple[float, float]) -> bool:
     """
     It is known that if bounding boxes
     of two segments do not intersect, segments do not intersect either.
@@ -159,13 +175,14 @@ def do_boxes_intersect(a: tuple, b: tuple, c: tuple, d: tuple) -> bool:
     return a[0] <= d[0] and b[0] >= c[0] and a[1] <= c[1] <= b[1]
 
 
-def intersects(segment_a: list or tuple, segment_b: list or tuple) -> bool:
+def intersects(segment_a: Sequence[Tuple[float, float]],
+               segment_b: Sequence[Tuple[float, float]]) -> bool:
     """
     If segment_a is [A, B] and segment_b is [C, D] then segments intersects if
     [A, B, D] is clockwise and [A, B, C] is counter-clockwise, or vice versa.
 
-    :param segment_a: list of tuples -- segment of first segment
-    :param segment_b: list of tuples -- segment of second segment
+    :param segment_a: List of tuples -- segment of first segment
+    :param segment_b: List of tuples -- segment of second segment
     :return: bool
     """
     a, b = segment_a
@@ -193,7 +210,7 @@ class Light:
     field-of-view simulation.
     """
 
-    def __init__(self, x: int, y: int, color: tuple, obstacles: list):
+    def __init__(self, x: int, y: int, color: Tuple, obstacles: List):
         self.origin = x, y  # position of the light/observer
         self.color = color
 
@@ -220,12 +237,12 @@ class Light:
         self.origin = x, y
 
     @staticmethod
-    def screen_borders_to_walls() -> tuple:
+    def screen_borders_to_walls() -> Tuple:
         """
         Screen-borders are outermost boundaries of our visibility-light
         polygon. No ray could surpass them.
 
-        :return: tuple
+        :return: Tuple
         """
         north_border = ((SCREEN_H, 0), (SCREEN_H, SCREEN_W))
         east_border = ((SCREEN_H, SCREEN_W), (0, SCREEN_W))
@@ -233,7 +250,7 @@ class Light:
         west_border = ((0, 0), (SCREEN_H, 0))
         return north_border, east_border, south_border, west_border
 
-    def obstacles_to_walls(self) -> tuple:
+    def obstacles_to_walls(self) -> Tuple:
         """
         Each obstacle should be a polygon, which is a list of segments
         represented by tuples (x, y) ordered counter-clockwise. We detect
@@ -255,7 +272,7 @@ class Light:
                 walls.append(wall)
         return tuple(walls)
 
-    def find_corners(self) -> list:
+    def find_corners(self) -> List:
         walls = self.walls
         corners = []
 
@@ -270,10 +287,10 @@ class Light:
         return corners
 
     @staticmethod
-    def get_border_corners() -> tuple:
+    def get_border_corners() -> Tuple:
         return (SCREEN_H, 0), (SCREEN_H, SCREEN_W), (0, SCREEN_W), (0, 0)
 
-    def calculate_walls_centers(self) -> dict:
+    def calculate_walls_centers(self) -> Dict:
         centers = {}
 
         for wall in self.walls + self.border_walls:
@@ -301,13 +318,13 @@ class Light:
         # finally, we build a visibility polygon using endpoint of each ray:
         self.light_polygon = [r[1] for r in rays]
 
-    def sort_walls(self, origin: tuple) -> list:
+    def sort_walls(self, origin: Tuple) -> List[Tuple[float, float]]:
         """
         Return wals sorted according to distance to origin and without walls
         which could not be seen/lit up since they are behind some, closer
         walls.
-        :param origin: tuple -- (x, y) location of light
-        :return: list -- sorted walls without redundant walls
+        :param origin: Tuple -- (x, y) location of light
+        :return: List -- sorted walls without redundant walls
         """
         walls = [w for w in self.walls]
         # sorting walls according to their distance to origin allows for
@@ -320,17 +337,19 @@ class Light:
         walls.sort(key=lambda w: w in self.border_walls)
         return walls
 
-    def collide_rays_w_walls(self, corners: list, origin: tuple, rays: list,
-                             walls: list):
+    def collide_rays_w_walls(self, corners: List[Tuple],
+                             origin: Tuple[float, float],
+                             rays: List[Tuple],
+                             walls: List[Tuple]) -> List[Tuple]:
         """
         Test for intersections of each ray and each wall of each obstacle to
         build final polygon representing our visible/lit-up area.
 
         :param corners: set -- vertices of all obstacles
-        :param origin: tuple -- (x, y) position of light/observer
-        :param rays: list -- all rays emitted from origin to corners
-        :param walls: list -- all walls which can block vision/light
-        :return: list -- clockwise-ordered points of visibility polygon
+        :param origin: Tuple -- (x, y) position of light/observer
+        :param rays: List -- all rays emitted from origin to corners
+        :param walls: List -- all walls which can block vision/light
+        :return: List -- clockwise-ordered points of visibility polygon
         """
         colliding = set()  # rays which intersects any wall
         offset_rays = []  # rays sweeping around obstacle's corners
@@ -358,31 +377,29 @@ class Light:
         return [r for r in rays if r not in colliding] + offset_rays
 
     @staticmethod
-    def filter_rays(origin: tuple, rays: list, wall: tuple) -> filter:
+    def filter_rays(origin: Tuple, rays: List, wall: Tuple) -> Iterator:
         """
         Find rays which could intersect with this wall, eg.: orientation of
         their ending to wall starting vertex is clockwise and to ending
         vertex is counterclockwise.
 
-        :param origin: tuple -- light source
-        :param rays: list -- all emitted rays
-        :param wall: tuple -- current wall to test against
-        :return: Iterator -- filtered rays
+        :param origin: Tuple -- light source
+        :param rays: List -- all emitted rays
+        :param wall: Tuple -- current wall to test against
+        :return: Iterator -- filter object (filtered rays)
         """
-        # first_step = filter(lambda r: ccw((origin, wall[1], r[1])), rays)
-        # return filter(lambda r: not ccw((origin, wall[0], r[1])), first_step)
         return filter(lambda r: ccw((origin, wall[1], r[1])) and
                       not ccw((origin, wall[0], r[1])), rays)
 
-    def create_rays_for_corners(self, origin: tuple, corners: list) -> list:
+    def create_rays_for_corners(self, origin: Tuple, corners: List) -> List:
         """
         Create a 'ray' connecting origin with each corner (obstacle vertex) on
         the screen. Ray is a tuple of two (x, y) coordinates used later to
         find which segment obstructs visibility.
         TODO: find way to emit less offset rays [x][ ]
-        :param origin: tuple -- point from which 'light' is emitted
-        :param corners: list -- vertices of obstacles
-        :return: list -- rays to be tested against obstacles edges
+        :param origin: Tuple -- point from which 'light' is emitted
+        :param corners: List -- vertices of obstacles
+        :return: List -- rays to be tested against obstacles edges
         """
         corners_open_walls = self.corners_open_walls
         corners_close_walls = self.corners_close_walls
--- main.py
+++ main.py
@@ -7,13 +7,15 @@ import cProfile
 import pstats
 import random
 import time
+
 from functools import wraps
+from typing import List, Tuple, Optional
 
 import pygame
 import pygame.freetype
 
-from options_screen import (BLACK, Button, CheckButton, ClampedValue, GREEN,
-    WHITE)
+from options_screen import (Button, CheckButton, ClampedValue, GREEN, BLACK,
+                            WHITE)
 
 # debug variables:
 TIMER = True
@@ -35,7 +37,6 @@ RANDOM_COLORS = False
 # to profile simulation on your machine, set this to True:
 PROFILE = False
 
-
 # constants:
 pygame.init()
 FONT = pygame.freetype.SysFont("Garamond", 20)
@@ -48,6 +49,7 @@ DARK = (32, 32, 32)
 LIGHT = (192, 192, 192)
 SHADOW = (240, 240, 240)
 
+# cached functions:
 get_time = time.perf_counter
 draw = pygame.draw
 draw_line = draw.line
@@ -55,9 +57,8 @@ draw_circle = draw.circle
 draw_polygon = draw.polygon
 draw_text = FONT.render_to
 
-
-gfps = []
-interactables = []
+displayed_fps = [0, 0]  # total number of measures, sum of measures
+options = []
 run_simulation = False
 
 
@@ -76,14 +77,17 @@ def timer(func):
         fps = 1 // total_time
 
         if func.__name__ == "update_lights":
-            global gfps
-            if len(gfps) == 10:
-                gfps.pop(0)
-            gfps.append(fps)
+            global displayed_fps
+            if displayed_fps[0] < 30:
+                displayed_fps[0] += 1
+                displayed_fps[1] += fps
+            else:
+                displayed_fps = [0, 0]
 
         fr = f"{func.__name__} finished in {total_time:.4f} secs. FPS: {fps}"
         print(fr)
         return result
+
     return wrapper
 
 
@@ -93,11 +97,12 @@ def main_loop():
     pointed = None
 
     while not run_simulation:
-        redraw_configuration_screen(interactables)
+        redraw_configuration_screen(options)
         for event in pygame.event.get():
             event_type = event.type
             if event_type == pygame.MOUSEMOTION:
-                pointed = if_mouse_over_interactable(*event.pos, interactables)
+                x, y = event.pos
+                pointed = if_mouse_over_clickable(x, y, options)
             elif event_type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # mouse left button
                     if pointed is not None:
@@ -115,27 +120,29 @@ def main_loop():
         for event in pygame.event.get():
             event_type = event.type
             if event_type == pygame.MOUSEMOTION and bind_light_to_cursor:
-                on_mouse_motion(*event.pos, lights)
+                x, y = event.pos
+                on_mouse_motion(x, y, lights)
             elif event_type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
                 if event.button == 1:  # mouse left button
                     bind_light_to_cursor = drag_or_drop(bind_light_to_cursor)
-                    on_mouse_motion(*event.pos, lights)
+                    on_mouse_motion(x, y, lights)
             elif event_type == pygame.QUIT:
                 run_simulation = False
                 pygame.quit()
 
 
-def create_obstacles(vertices=OBSTACLE_EDGES):
+def create_obstacles() -> List:
     obstacles, bounding_boxes = [], []
     size = OBSTACLE_EDGE_SIZE
-    for i in range(size*2, SCREEN_W, size*3):
-        for j in range(size*2, SCREEN_H, size*3):
+    for i in range(size * 2, SCREEN_W, size * 3):
+        for j in range(size * 2, SCREEN_H, size * 3):
             obstacle = new_obstacle(i, j)
             obstacles.append(obstacle)
     return obstacles
 
 
-def new_obstacle(i, j):
+def new_obstacle(i: float, j: float) -> List:
     """Produce obstacle (polygon) of any size and number of vertices."""
     obstacle = []
     size = OBSTACLE_EDGE_SIZE
@@ -143,7 +150,7 @@ def new_obstacle(i, j):
     for k in range(edges):
         angle = (k - 1) * (360 // edges)
         offset = 180 // edges
-        point = move_along_vector((i, j), size, angle=angle-offset)
+        point = move_along_vector((i, j), size, angle=angle - offset)
         obstacle.append(point)
     return obstacle
 
@@ -154,7 +161,7 @@ def update_lights(lights):
         light.update_visible_polygon()
 
 
-def create_lights(obstacles):
+def create_lights(obstacles: List) -> List:
     lights = []
     x, y = SCREEN_W // 2, SCREEN_H // 2
     for i in range(LIGHTS_COUNT):
@@ -166,7 +173,7 @@ def create_lights(obstacles):
     return lights
 
 
-def get_light_color():
+def get_light_color() -> Tuple:
     if RANDOM_COLORS:
         randint = random.randint
         return randint(0, 255), randint(0, 255), randint(0, 255)
@@ -174,7 +181,7 @@ def get_light_color():
         return LIGHT
 
 
-def get_light_position(i, x, y):
+def get_light_position(i: int, x: float, y: float) -> Tuple:
     if not i:
         point = (x, y)
     else:
@@ -185,10 +192,10 @@ def get_light_position(i, x, y):
 
 # noinspection PyTypeChecker
 def create_interactable_options():
-    global interactables
+    global options
     p = (SCREEN_H / 2, SCREEN_W / 2)  # basic position to override
     positions = [(p[0] + 300, p[1]), (p[0], p[1] - 100), (p[0], p[1] - 200),
-                 (p[0], p[1] - 300), (p[0], p[1] - 400)]
+        (p[0], p[1] - 300), (p[0], p[1] - 400)]
     types = [Button, ClampedValue, ClampedValue, CheckButton, CheckButton]
     functions = [run_application, change_edges_count, change_edge_size,
         toggle_rays, toggle_colors]
@@ -198,15 +205,15 @@ def create_interactable_options():
 
     for i, position in enumerate(positions):
         if types[i] == ClampedValue:
-            min, max, step = ranges[i]
-            b = ClampedValue(window, *position, 25, 25, values[i], min, max,
-                         step, functions[i], labels[i])
+            _min, _max, step = ranges[i]
+            b = ClampedValue(window, *position, 25, 25, values[i], _min, _max,
+                             step, functions[i], labels[i])
         elif types[i] == CheckButton:
             b = CheckButton(window, *position, 15, 15, values[i], functions[i],
                             labels[i])
         else:
             b = Button(window, *position, 25, 25, values[i], functions[i])
-        interactables.append(b)
+        options.append(b)
 
 
 def run_application():
@@ -215,27 +222,26 @@ def run_application():
 
 
 def change_edges_count():
-    global interactables, OBSTACLE_EDGES
-    OBSTACLE_EDGES = interactables[1].value
+    global options, OBSTACLE_EDGES
+    OBSTACLE_EDGES = options[1].value
 
 
 def change_edge_size():
     global OBSTACLE_EDGE_SIZE
-    OBSTACLE_EDGE_SIZE = interactables[2].value
+    OBSTACLE_EDGE_SIZE = options[2].value
 
 
 def toggle_rays():
     global SHOW_RAYS
-    SHOW_RAYS = interactables[3].value
-    print(SHOW_RAYS)
+    SHOW_RAYS = options[3].value
 
 
 def toggle_colors():
     global RANDOM_COLORS
-    RANDOM_COLORS = interactables[4].value
+    RANDOM_COLORS = options[4].value
 
 
-def redraw_configuration_screen(interactables: list):
+def redraw_configuration_screen(clickable_items: List):
     """
     Draw configuration screen, where user can set-up key variables of the
     simulation.
@@ -245,53 +251,50 @@ def redraw_configuration_screen(interactables: list):
     poly = new_obstacle(SCREEN_W // 2, SCREEN_H - 300)
     draw_polygon(window, WHITE, poly)
 
-    for interactable in interactables:
-        interactable.draw()
+    for item in clickable_items:
+        item.draw()
     pygame.display.update()
 
 
-def if_mouse_over_interactable(x, y, interactables: list):
+def if_mouse_over_clickable(x: float,
+                            y: float,
+                            clickable_items: List) -> Optional[Button]:
     pointed = None
-    for interactable in interactables:
-        if interactable.mouse_over(x, y):
-            interactable.active = True
-            return interactable
+    for item in clickable_items:
+        if item.mouse_over(x, y):
+            item.active = True
+            return item
     else:
-        for interactable in interactables:
-            interactable.active = False
+        for item in clickable_items:
+            item.active = False
     return pointed
 
 
-# @timer
-def redraw_screen(lights, obstacles):
+def redraw_screen(lights: List, obstacles: List):
     window.fill(DARK)
     draw_obstacles(obstacles)
     draw_light(lights)
-    if gfps:
+    if displayed_fps:
         draw_fps_counter()
     pygame.display.update()
 
 
 def draw_fps_counter():
-    value = sum(gfps) // len(gfps)
+    try:
+        value = (displayed_fps[1] // displayed_fps[0])
+    except ZeroDivisionError:
+        return
     color = GREEN if value > 24 else RED
     text = "FPS: " + str(value)
     draw_text(window, (SCREEN_W // 2, SCREEN_H // 20), text, color)
 
 
-def draw_intersection(intersector, second_segemnt):
-    color = RED if intersects(intersector, second_segemnt) else WHITE
-    draw_line(window, color, *intersector)
-    draw_line(window, color, *second_segemnt)
-
-
-def draw_obstacles(obstacles):
+def draw_obstacles(obstacles: List):
     for obstacle in obstacles:
         draw_polygon(window, BLACK, obstacle)
 
 
-# @timer
-def draw_light(lights):
+def draw_light(lights: List):
     for light in lights:
         if len(light.light_polygon) > 2:
             draw_polygon(window, light.color, light.light_polygon)
@@ -304,7 +307,7 @@ def draw_light(lights):
         draw_circle(window, BLACK, (int(x), int(y)), 5)
 
 
-def on_mouse_motion(x, y, lights):
+def on_mouse_motion(x: float, y: float, lights: List):
     lights[0].move_to(x, y)
     if len(lights) > 1:
         for i, light in enumerate(lights):
@@ -320,7 +323,7 @@ def drag_or_drop(drag_light):
 
 if __name__ == "__main__":
     # do not move these imports to the top!
-    from geometry import Light, intersects, move_along_vector
+    from geometry import Light, move_along_vector
 
     window = pygame.display.set_mode((SCREEN_W, SCREEN_H))
     pygame.display.set_caption(TITLE)
